4990bcbf181cab6f8d2eb8ac56339a7b16ba49a5
DuHyeon <engus983@gmail.com>
2019-05-27 23:34:55 +0900

    STYLE: #5.1 Footer and Auth UI part Two
    Branch the action that logIn and signUp

--- origin/src/Components/App.js
+++ origin/src/Components/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
 import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
 // Client Query
 const QUERY = gql`
   {
     isLoggedIn @client
   }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
 `;
 
 export default () => {
@@ -22,10 +29,11 @@ export default () => {
   return (
     <ThemeProvider theme={Theme}>
       {/* div랑 같은 뜻 */}
-      <>
+      <Wrapper>
         <GlobalStyles />
         <AppRouter isLoggedIn={isLoggedIn} />
-      </>
+        <Footer />
+      </Wrapper>
     </ThemeProvider>
   );
 };
